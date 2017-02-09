9f261ea31d4f20fc8ee310f6c7b16e602d543756
Ростислав <extra0@mail.ru>
2017-02-09 21:45:39 +0200

    checkpoint

--- develop/js/script.js
+++ develop/js/script.js
@@ -2,4 +2,14 @@ $(function(){
 
 	// вызов фенсибокса
 	$('[fancybox]').fancybox();
+
+
+	// слайдер на главной
+	$('.main-slider__list').bxSlider({
+		mode: 'fade',
+		controls: false,
+		auto: true
+	});
+
+
 });
