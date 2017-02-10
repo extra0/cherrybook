2944f6cf74c69b39ab783d91339cff45352e288e
Ростислав <extra0@mail.ru>
2017-02-10 23:07:19 +0200

    главная без адаптива

--- develop/js/script.js
+++ develop/js/script.js
@@ -11,5 +11,21 @@ $(function(){
 		auto: true
 	});
 
+	// слайдер книги
+	$('.form-block__inner-slider').bxSlider({
+		controls: false,
+		auto: true,
+		autoHeight: true
+	});
+
+	// табы формата
+	$('.format-block__tab-list-item').each(function(i){ $(this).attr('data-tab', i);});
+	$('.format-block__tab-block').each(function(i){ $(this).attr('data-tab', i);});
+
+	$('.format-block__tab-list-item').on('click', function(){
+		$('.format-block__tab-list-item, .format-block__tab-block').removeClass('active');
+		$(this).addClass('active');
+		$('.format-block__tab-block[data-tab='+ $(this).attr('data-tab') +']').addClass('active');
+	});
 
 });
